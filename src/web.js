e1e6a609302197dab8482b981df1e7fe2efb8827
Cecile Muller <git@wildpeaks.fr>
2017-01-17 15:06:35 +0100

    Removed arrow functions
    Because this example config doesn't use Babel

--- src/web.js
+++ src/web.js
@@ -1,9 +1,9 @@
 /* eslint-env browser */
 'use strict';
-
 console.log('[MAIN] Started');
+
 const worker = new Worker('worker.bundle.js');
-worker.onmessage = e => {
+worker.onmessage = function(e){
 	console.log('[MAIN]', e);
 };
 worker.postMessage({
--- src/worker.js
+++ src/worker.js
@@ -1,8 +1,8 @@
 /* eslint-env worker */
 'use strict';
-
 console.log('[WORKER] Started');
-self.onmessage = e => {
+
+self.onmessage = function(e){
 	console.log('[WORKER]', e);
 	self.postMessage({
 		hello: 222
